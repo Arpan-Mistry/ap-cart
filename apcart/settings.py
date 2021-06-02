4397ddb6d226d0623cf1bb633c4d7285c1b77d02
arpan <evisionatt@gmail.com>
2021-06-02 11:10:02 +0530

    make it better

--- apcart/settings.py
+++ apcart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,7 @@ EMAIL_HOST_PASSWORD = 'apcart@000' #past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
+
+
+
+django_heroku.settings(locals())
